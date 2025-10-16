0dd53fd27b3c699021e0a9903a247ffb8a308e65
Tolga Yanar <tolga.yanar@ug.bilkent.edu.tr>
2025-10-16 17:13:21 +0300

    local storage update on usepopcorn project

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -98,10 +98,15 @@ const KEY = "426bacf6";
 export default function App() {
   const [query, setQuery] = useState("inception");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
+
   // useEffect(function () {
   //   console.log("After inital render");
   // }, []);
@@ -134,6 +139,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
